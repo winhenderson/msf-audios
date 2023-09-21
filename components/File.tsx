ef9b5e252ed20b38f120d9e3d449f06e53f8359f
Winfield Henderson <109438943+winhenderson@users.noreply.github.com>
2023-09-21 14:38:56 -0400

    actually maybe got the download thing working

--- components/File.tsx
+++ components/File.tsx
@@ -1,17 +1,8 @@
-import axios, {
-  AxiosResponse,
-  RawAxiosResponseHeaders,
-  AxiosResponseHeaders,
-} from "axios";
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import axios from "axios";
+import React from "react";
 
 const File: React.FC<{ path: string }> = ({ path }) => {
   const url = `https://msf-audios.nyc3.digitaloceanspaces.com/${path}`;
-  // const { download } = useDownloadFile(async () =>
-  //   blobMapper(await axios.get(url))
-  // );
-
-  // console.log(download());
   return (
     <li>
       <button
@@ -19,7 +10,7 @@ const File: React.FC<{ path: string }> = ({ path }) => {
           axios({
             url: url,
             method: "GET",
-            responseType: "blob", // important
+            responseType: "blob",
           }).then((response) => {
             const blob = new Blob([response.data], {
               type: response.data.type,
@@ -48,81 +39,3 @@ const File: React.FC<{ path: string }> = ({ path }) => {
 };
 
 export default File;
-
-interface DownloadableResponse {
-  blob: Blob;
-  filename: string;
-}
-interface FileDownload {
-  download: () => void;
-}
-
-export const useDownloadFile = (
-  request: () => Promise<DownloadableResponse>
-): FileDownload => {
-  const anchorRef = useRef<HTMLAnchorElement | null>(null);
-  const [url, setUrl] = useState("");
-
-  useEffect(() => {
-    const anchor = document.createElement("a");
-    anchor.style.display = "none";
-    anchorRef.current = anchor;
-    document.body.appendChild(anchor);
-
-    return () => {
-      document.body.removeChild(anchor);
-    };
-  }, []);
-
-  useEffect(() => {
-    const toRevoke = url;
-    return () => {
-      if (toRevoke) {
-        URL.revokeObjectURL(toRevoke);
-      }
-    };
-  }, [url]);
-
-  const download = useCallback(() => {
-    request()
-      .then(({ blob, filename }) => {
-        console.log("filename::::", filename);
-        const url = URL.createObjectURL(blob);
-        setUrl(url);
-        if (anchorRef.current) {
-          anchorRef.current.href = url;
-          anchorRef.current.download = filename;
-          anchorRef.current.click();
-        }
-      })
-      .catch(() => {
-        // Handle error here
-      });
-  }, [request]);
-
-  return { download };
-};
-
-const blobMapper = (
-  response: AxiosResponse<BlobPart>
-): DownloadableResponse => ({
-  blob: new Blob([response.data]),
-  filename: getFilenameFromHeaders(response.headers),
-});
-
-const CONTENT_DISPOSITION_HEADER = "content-disposition";
-
-export const getFilenameFromHeaders = (
-  headers: RawAxiosResponseHeaders | AxiosResponseHeaders
-): string => {
-  const headerLine = headers[CONTENT_DISPOSITION_HEADER];
-  if (!headerLine) {
-    throw new Error(`could not find '${CONTENT_DISPOSITION_HEADER}' header`);
-  }
-
-  try {
-    return headerLine.split("filename=")[1].replace(/"/g, "");
-  } catch {
-    throw new Error(`could not parse '${CONTENT_DISPOSITION_HEADER}' header`);
-  }
-};
