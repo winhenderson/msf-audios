fef56c62e3aa20d8a0c63793930c49bafff423f2
Winfield Henderson <109438943+winhenderson@users.noreply.github.com>
2023-09-21 17:09:54 -0400

    added some info to files

--- components/File.tsx
+++ components/File.tsx
@@ -1,41 +1,70 @@
+import { DateString } from "@/pages";
 import axios from "axios";
 import React from "react";
 
-const File: React.FC<{ path: string }> = ({ path }) => {
-  const url = `https://msf-audios.nyc3.digitaloceanspaces.com/${path}`;
+type Props = {
+  path: string;
+  lastModified: DateString;
+  size: number;
+};
+
+const File: React.FC<Props> = ({ path, lastModified, size }) => {
+  const date = new Date(lastModified);
+  const dateString = date.toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
+  const timeString = date.toLocaleTimeString("en-us", {
+    hour: "numeric",
+    minute: "2-digit",
+  });
+  const finalString = `${dateString} ${timeString}`;
+
   return (
-    <li>
+    <li className="flex  h-14 shadow-md mb-1 text-left px-4  justify-between items-center hover:bg-gray-100">
       <button
-        onClick={(e) =>
-          axios({
-            url: url,
-            method: "GET",
-            responseType: "blob",
-          }).then((response) => {
-            const blob = new Blob([response.data], {
-              type: response.data.type,
-            });
-            const url = window.URL.createObjectURL(blob);
-            const link = document.createElement("a");
-            link.href = url;
-            const contentDisposition = response.headers["content-disposition"];
-            let fileName = path;
-            if (contentDisposition) {
-              const fileNameMatch = contentDisposition.match(/filename="(.+)"/);
-              if (fileNameMatch.length === 2) fileName = fileNameMatch[1];
-            }
-            link.setAttribute("download", fileName);
-            document.body.appendChild(link);
-            link.click();
-            link.remove();
-            window.URL.revokeObjectURL(url);
-          })
-        }
+        onClick={(e) => {
+          e.preventDefault();
+          download(path);
+        }}
       >
         {path}
       </button>
+      <div className="text-gray-500 flex justify-around basis-1/3">
+        <span>{finalString}</span>
+        <span>{(size / 1000000).toFixed(1)} MB</span>
+      </div>
     </li>
   );
 };
 
 export default File;
+
+function download(path: string) {
+  const url = `https://msf-audios.nyc3.digitaloceanspaces.com/${path}`;
+  axios({
+    url: url,
+    method: "GET",
+    responseType: "blob",
+  }).then((response) => {
+    const blob = new Blob([response.data], {
+      type: response.data.type,
+    });
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    const contentDisposition = response.headers["content-disposition"];
+    let fileName = path;
+    if (contentDisposition) {
+      const fileNameMatch = contentDisposition.match(/filename="(.+)"/);
+      if (fileNameMatch.length === 2) fileName = fileNameMatch[1];
+    }
+    link.setAttribute("download", fileName);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    window.URL.revokeObjectURL(url);
+  });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,72 @@ import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 import * as cloud from "@friends-library/cloud";
 import File from "@/components/File";
 
+export type metaData = ReturnType<typeof cloud.metaData>;
+
+export type DateString = string;
+
 type Props = {
-  files: string[];
+  usefulInfo: Array<{
+    fileName: string;
+    lastModified: DateString;
+    size: number;
+  }>;
 };
 
 export const getServerSideProps = (async (context) => {
   const fileNames = await cloud.listObjects("");
+  const promisedData: metaData[] = [];
   for (const file of fileNames) {
-    const data = await cloud.metaData(file);
-    // console.log(data);
+    promisedData.push(cloud.metaData(file));
   }
-  return { props: { files: fileNames } };
+  const data = await Promise.all(promisedData);
+  console.log("filenames", fileNames.length);
+  const usefulInfo = [];
+  for (let i = 0; i < fileNames.length; i++) {
+    console.log("in here!");
+    let foo = {
+      fileName: fileNames[i],
+      lastModified:
+        data[i].LastModified?.toISOString() ?? new Date().toISOString(),
+      size: data[i].ContentLength ?? 0,
+    };
+    console.log("foo:", foo);
+    usefulInfo.push(foo);
+  }
+  console.log(":::", usefulInfo);
+  return { props: { usefulInfo } };
 }) satisfies GetServerSideProps<Props>;
 
-export default function Page({
-  files,
+//
+//
+//
+//
+//
+
+export function Page({
+  usefulInfo,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.info({ usefulInfo });
   return (
-    <ul>
-      {files.map((file) => (
-        <File path={file} key={crypto.randomUUID()} />
-      ))}
-    </ul>
+    <>
+      <ul className="flex flex-col w-3/4">
+        <li className="flex  h-14 justify-between items-center mb-2 border-b-gray-400 border-b-2 border-b-solid px-4 text-lg">
+          <span>Name</span>
+          <div className="flex justify-around basis-1/3">
+            <span className="-ml-6">Last Modified</span>
+            <span className="-ml-1">Size</span>
+          </div>
+        </li>
+        {usefulInfo.map((info) => (
+          <File
+            path={info.fileName}
+            key={crypto.randomUUID()}
+            lastModified={info.lastModified}
+            size={info.size}
+          />
+        ))}
+      </ul>
+    </>
   );
 }
+export default Page;
